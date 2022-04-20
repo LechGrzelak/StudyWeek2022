44deb53d7962e50a62eb9ceaef8525c657f1688b
LuisSouto <luisouto.94@gmail.com>
2022-04-20 12:10:55 +0200

    Add files via upload
    Call pricer added.

--- montecarlo.py
+++ montecarlo.py
@@ -1,6 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import py_vollib.black_scholes.implied_volatility as bs
 
+
+def call_price(S,S0,K,T,r):
+    C = np.maximum(S-K,0).mean()
+    # return C
+    return bs.implied_volatility(C,S0,K,T,r,'c')
 
 def SABR_S(S0: float,v0: float,beta: float,dW: float):
     # print(S0)
@@ -22,16 +28,18 @@ def SABR(S0: float,alpha: float,beta: float,gamma: float,rho: float,T: float,n_p
     v = np.ones(n_p)*alpha
     for i in range(n_t):
         S,v = SABR_step(S,v,gamma,beta,rho,dt)
-        S -= S.mean() - S0
         # print(S)
+    S -= S.mean() - S0
 
     return S 
 
 
 S0 = 1
-alpha = 1
+K  = S0
+r  = 0.1
+alpha = 0.1
 beta = 1
-gamma = 1
+gamma = 0.5
 rho = 0
 T = 1
 n_p = 1000
@@ -39,3 +47,4 @@ n_t = 100
 
 S = SABR(S0,alpha,beta,gamma,rho,T,n_p,n_t)
 print(np.mean(S))
+print(call_price(S,S0,K,T,r))
